82b42a61beed289e13bb0c74e47f062c39744945
Sam Arnold <sarnold@influxdata.com>
2022-07-15 23:12:17 -0400

    wip: ingest bulk working

--- influxdb_iox/src/commands/bulk_ingest.rs
+++ influxdb_iox/src/commands/bulk_ingest.rs
@@ -1,26 +1,26 @@
 use std::collections::BTreeMap;
 use std::fs::File;
+use std::io;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 use std::sync::Arc;
 use std::time::{Duration, Instant};
 use itertools::Itertools;
-use object_store::DynObjectStore;
-use object_store::memory::InMemory;
 use snafu::{ResultExt, Snafu};
-use tokio::time::sleep;
-use data_types::{KafkaPartition, Sequence, SequenceNumber};
-use iox_catalog::interface::Catalog;
-use iox_catalog::mem::MemCatalog;
+use data_types::{DatabaseName, KafkaPartition, Namespace, PartitionTemplate, Sequence, SequenceNumber, TemplatePart};
 use iox_query::exec::Executor;
 use mutable_batch_lp;
 use data_types::NamespaceSchema;
 use iox_catalog::validate_or_insert_schema;
 use iox_time::{SystemProvider, Time};
 use backoff::{BackoffConfig};
+use clap_blocks::catalog_dsn::CatalogDsnConfig;
+use clap_blocks::object_store::{make_object_store, ObjectStoreConfig};
 use dml::{DmlMeta, DmlOperation, DmlWrite};
-use ingester::data::{IngesterData, SequencerData};
-use ingester::lifecycle::{LifecycleConfig, LifecycleHandle, LifecycleManager};
+use ingester::data::{IngesterData, Persister, SequencerData};
+use ingester::lifecycle::{LifecycleConfig, LifecycleManager};
+use iox_catalog::interface::Catalog;
+use router::dml_handlers::{DmlHandler, Partitioner};
 
 /// Bulk ingest data without saving it in kafka
 ///
@@ -30,14 +30,24 @@ pub struct Config {
     /// File with data to load. Currently supported formats are .lp
     #[clap(action)]
     file_name: PathBuf,
+
+    #[clap(flatten)]
+    object_store: ObjectStoreConfig,
+
+    #[clap(flatten)]
+    catalog_dsn: CatalogDsnConfig,
 }
 
 #[derive(Debug, Snafu)]
 pub enum Error {
-
     #[snafu(display("File io failed: {}", source))]
     FileIo {
         source: std::io::Error,
+    },
+
+    #[snafu(display("Catalog read failed: {}", source))]
+    CatalogRead {
+        source: iox_catalog::interface::Error,
     },
 
     #[snafu(display("Converting LP to mutable batch failed: {}", source))]
@@ -45,30 +55,89 @@ pub enum Error {
         source: mutable_batch_lp::Error,
     },
 
-    #[snafu(display("Persisting mutable batch failed {}", source))]
+    #[snafu(display("Persisting mutable batch failed: {}", source))]
     Ingester {
         source: ingester::data::Error,
-    }
+    },
+
+    #[snafu(display("Partitioning write batch failed: {}", source))]
+    Partitioner {
+        source: router::dml_handlers::PartitionError,
+    },
+
+    #[snafu(display("Creating object store from config failed: {}", source))]
+    ObjectStoreParse {
+        source: clap_blocks::object_store::ParseError,
+    },
+
+    #[snafu(display("Creating catalog from config failed: {}", source))]
+    CatalogParse {
+        source: clap_blocks::catalog_dsn::Error,
+    },
+
 }
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
+pub async fn persist_all(catalog : &dyn Catalog, namespace : &Namespace, data: &IngesterData) -> Result<()> {
+    let start = Instant::now();
+    println!("persisting partitions");
+    let mut repos = catalog.repositories().await;
+    let partitions = repos.partitions().list_by_namespace(namespace.id).await.context(CatalogReadSnafu)?;
+    drop(repos);
+    for partition in partitions {
+        data.persist(partition.id).await;
+    }
+    let end = Instant::now();
+    println!("persisted partitions {}s", (end - start).as_secs());
+    Ok(())
+}
+
 pub async fn command(config: Config) -> Result<()> {
-    let file = File::open(config.file_name).context(FileIoSnafu)?;
-    let file = BufReader::with_capacity(1000000, file);
-    let lines = &mut file.lines().peekable();
+    let mut stdin_lines;
+    let mut buf_lines;
+    let lines : &mut dyn Iterator<Item=_> = if config.file_name.to_string_lossy() == "-" {
+        stdin_lines = io::stdin().lock().lines();
+        &mut stdin_lines
+    } else {
+        let file = File::open(config.file_name).context(FileIoSnafu)?;
+        buf_lines = BufReader::with_capacity(10000000, file).lines();
+        &mut buf_lines
+    };
 
     let metrics = Arc::new(metric::Registry::new());
-    let catalog: Arc<dyn Catalog> = Arc::new(MemCatalog::new(Arc::clone(&metrics)));
+    let object_store = make_object_store(&config.object_store).context(ObjectStoreParseSnafu)?;
+    let catalog = config.catalog_dsn
+        .get_catalog("iox_objectstore_garbage_collect", metrics.clone())
+        .await.context(CatalogParseSnafu)?;
+    println!("using catalog: {:?}", config.catalog_dsn);
+    println!("using object store: {:?}", config.object_store);
+
+    let namespace_name = "0000000000000001_0000000000000002";
+
+    let kafka_topic;
+    let query_pool;
+    let namespace;
+    {
+        let mut transaction = catalog.start_transaction().await.unwrap();
+        kafka_topic = transaction.kafka_topics().create_or_get("whatevs").await.unwrap();
+        query_pool = transaction.query_pools().create_or_get("whatevs").await.unwrap();
+        namespace = match transaction.namespaces().get_by_name(namespace_name).await.unwrap() {
+            Some(n) => n,
+            None => {
+                transaction.namespaces()
+                    .create(namespace_name, "inf", kafka_topic.id, query_pool.id)
+                    .await
+                    .unwrap()
+            }
+        };
+        transaction.commit().await.unwrap();
+    }
+
     let mut repos = catalog.repositories().await;
-    let kafka_topic = repos.kafka_topics().create_or_get("whatevs").await.unwrap();
-    let query_pool = repos.query_pools().create_or_get("whatevs").await.unwrap();
+
     let kafka_partition = KafkaPartition::new(0);
-    let namespace = repos
-        .namespaces()
-        .create("0000000000000001_0000000000000002", "inf", kafka_topic.id, query_pool.id)
-        .await
-        .unwrap();
+
     let sequencer1 = repos
         .sequencers()
         .create_or_get(&kafka_topic, kafka_partition)
@@ -79,7 +148,6 @@ pub async fn command(config: Config) -> Result<()> {
         sequencer1.id,
         SequencerData::new(sequencer1.kafka_partition, Arc::clone(&metrics)),
     );
-    let object_store: Arc<DynObjectStore> = Arc::new(InMemory::new());
     let executor = Arc::new(Executor::new(1));
     let data = Arc::new(IngesterData::new(
         Arc::clone(&object_store),
@@ -92,60 +160,83 @@ pub async fn command(config: Config) -> Result<()> {
 
     let ignored_ts = Time::from_timestamp_millis(42);
 
-    let manager = LifecycleManager::new(
-        LifecycleConfig::new(1, 0, 0, Duration::from_secs(1), Duration::from_secs(1)),
+    let gb = 1000 * 1000 * 1000;
+    let mut manager = LifecycleManager::new(
+        LifecycleConfig::new(
+            4*gb,
+            2*gb,
+            1*gb,
+            Duration::from_secs(120),
+            Duration::from_secs(10),
+        ),
         metrics,
         Arc::new(SystemProvider::new()),
     );
+
+    let partitioner = Partitioner::new(PartitionTemplate {
+        parts: vec![TemplatePart::TimeFormat("%Y-%m-%d".to_owned())],
+    });
+
+    let database_name = DatabaseName::new("0000000000000001_0000000000000002").expect("valid database name");
+
     drop(repos);
 
+    let lines_per_loop : i64 = 100000;
+    let mut sequence : i64 = 0;
     loop {
+        if sequence % 100 == 0 {
+            println!("Finished {} lines", sequence*lines_per_loop)
+        }
+        sequence += 1;
         let mut err = Ok(());
 
-        let lp = lines.take(100000).filter_map(|x| match x {
+        let lp = lines.take(lines_per_loop as usize).filter_map(|x| match x {
             Ok(s) => Some(s),
-            Err(e) => {err = Err(e); None}
+            Err(e) => {
+                err = Err(e);
+                None
+            }
         }).join("\n");
         err.context(FileIoSnafu)?;
         if lp.is_empty() {
             break;
         }
-        let start = Instant::now();
-        println!("creating batch");
         let batches = mutable_batch_lp::lines_to_batches(&lp, 0).context(MutableBatchSnafu)?;
-        let end = Instant::now();
-        println!("created batch duration {}s", (end-start).as_secs());
-        let start = end;
-        let w1 = DmlWrite::new(
-            "0000000000000001_0000000000000002",
-            batches,
-            Some("1970-01-01".into()), //FIXME: bad partition key
-            DmlMeta::sequenced(
-                Sequence::new(1, SequenceNumber::new(1)),
-                ignored_ts,
-                None,
-                50,
-            ),
-        );
-        validate_or_insert_schema(w1.tables(), &schema, &mut *catalog.repositories().await)
-            .await
-            .expect("Schema failed validation");
-        let end = Instant::now();
-        println!("validated schema duration {}s", (end-start).as_secs());
-        let start = end;
 
-        let mut should_pause = data.buffer_operation(sequencer1.id, DmlOperation::Write(w1), &manager.handle()).await.context(IngesterSnafu)?;
-        if should_pause {
-            println!("Paused buffer ingest!")
-        }
-        while should_pause {
-            sleep(Duration::from_millis(100)).await;
-            should_pause = manager.handle().can_resume_ingest();
+        let partitioned = partitioner.write(&database_name.clone(), batches, None).await.context(PartitionerSnafu)?;
+
+        let writes: Vec<_> = partitioned.into_iter().map(|p| {
+            let (partition_key, partition_batches) = p.into_parts();
+            DmlWrite::new(
+                database_name.clone(),
+                partition_batches,
+                Some(partition_key),
+                DmlMeta::sequenced(
+                    Sequence::new(1, SequenceNumber::new(sequence)),
+                    ignored_ts,
+                    None,
+                    50,
+                ),
+            )
+        }).collect();
+
+        for write in &writes {
+            validate_or_insert_schema(write.tables(), &schema, &mut *catalog.repositories().await)
+                .await
+                .expect("Schema failed validation");
         }
 
-        let end = Instant::now();
-        println!("ingested duration {}s", (end-start).as_secs());
+        for write in writes {
+            let should_pause = data.buffer_operation(sequencer1.id, DmlOperation::Write(write), &manager.handle()).await.context(IngesterSnafu)?;
+            if should_pause {
+                let start = Instant::now();
+                manager.maybe_persist(&data).await;
+                let end = Instant::now();
+                println!("persisted partitions to dump memory {}s", (end - start).as_secs());
+            }
+        }
     }
+    persist_all(&*catalog, &namespace, &data).await?;
     executor.join().await;
     Ok(())
 }
